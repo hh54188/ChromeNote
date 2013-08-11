1d1f173613c780616d4d6615e5cb9f2e53bf757a
invegas <juststayinvegas@gmail.com>
2013-08-11 23:59:10 +0800

    I don't remember

--- devtools.js
+++ devtools.js
@@ -1,8 +1,8 @@
 chrome.devtools.panels.create(
     "TheNameOfYourExtension", 
-    "img/icon16.png", 
-    "index.html",
+    "icons/16x16.png", 
+    "devtools/index.html",
     function() {
- 
+        console.log("This is a test page");
     }
 );
