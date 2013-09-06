c5b53d6fadf818e4ae8896f3a6ea28e8cfcc028c
hh54188 <hh54188@yahoo.cn>
2013-09-06 10:59:20 +0800

    update

--- js/View/commandmenu.js
+++ js/View/commandmenu.js
@@ -1,19 +1,22 @@
-var CommandMenuView = Class.extend({
-    staticProperty: {
-        isOpen: false
-    },
-    staticMethods: {
-        show: function () {
-            if (this.isOpen) {
-                this.hide();
-            }
-            this.isOpen = true
-            log("show menu");
-        },
-        hide: function () {
-            if (!this.isOpen) return;
-            this.isOpen = false;
-            log("hide menu");
-        }
+var CommandMenuView = (function () {
+    var initialize = false;
+
+    var config = {
+        theme: "white",
+        showTip: true,
+        threeD: false
     }
-})
+
+    var open = function () {
+
+    }
+
+    var close = function () {
+
+    }
+
+    return {
+        open: open,
+        close: close
+    }
+})()
