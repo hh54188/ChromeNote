4cb3de6c05d149e966d645ec7c7568b2a3eb2bbb
invegas <juststayinvegas@gmail.com>
2013-11-20 00:34:44 +0800

    update

--- devtools/editor/js/Tip.js
+++ devtools/editor/js/Tip.js
@@ -1,12 +1,45 @@
 var Tip = (function () {
 
     var tpl = [
-        '<div class="tip-item arrow_box">',
+        '<div class="tip-item-wrap">',
+            '<div class="tip-item arrow_box"></div>',
+            '<div class="tip-btn-group">',
+                '<button class="tip-btn-del tip-btn"><i class="fa fa-trash-o"></i></button>',
+                '<button class="tip-btn-share tip-btn"><i class="fa fa-share"></i></button>',
+                '<button class="tip-btn-share-item tip-btn margin-left-30" ><i class="fa fa-twitter"></i></button>',
+                '<button class="tip-btn-share-item tip-btn"><i class="fa fa-facebook"></i></button>',
+                '<button class="tip-btn-share-item tip-btn"><i class="fa fa-google-plus"></i></button>',
+            '</div>',
         '</div>'
-    ].join(",");
+    ].join("");
 
-    var generate = function (html) {
+    var doms = {
+        body: $("body")
+    }
 
+    var bindEvent = function (item) {
+        var btnGroup = item.find(".tip-btn-group");
+        var shareItem = item.find(".tip-btn-share-item");
+        var shareBtn = item.find(".tip-btn-share");
+
+        item.on("mouseover", function () {
+            btnGroup.show();
+        }).on("mouseout", function () {
+            btnGroup.hide();
+        });
+
+        shareBtn.on("mouseover", function () {
+            
+        })
+    }
+
+    var generate = function (innerHTML) {
+        var item = $(tpl);
+        var content = item.find(".tip-item");
+        content.html(innerHTML);
+
+        doms.body.append(item);
+        bindEvent(item);
     }
 
     return {
